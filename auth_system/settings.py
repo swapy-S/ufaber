4d6296783b5c868a56d938b9811c1b0ecd6d4c12
Swapnil Rana <50835521+swapy-S@users.noreply.github.com>
2021-06-26 20:13:24 +0530

    Update settings.py

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = MEDIA_DIR
 # MEDIA_URL = ‘/media/’
